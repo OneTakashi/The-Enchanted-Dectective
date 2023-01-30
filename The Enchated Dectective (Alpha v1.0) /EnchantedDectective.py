d41281a51091c9ec8dd2a69f822862b13a3de8ab
Aaron <takashididnot@gmail.com>
2023-01-30 18:00:32 +0000

    ED Alpha v1.0

---
+++ The Enchated Dectective (Alpha v1.0) /EnchantedDectective.py
@@ -0,0 +1,162 @@
+
+#Starting Notes (Stable)
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+
+
+print("The Enchanted Detectice (A Python Based Decison Game)")
+print("")
+print(" ¬ Use VS Code with the Terminal in Fullscreen for the best experience")
+print(" ¬ *But most text editors are completely compatible too i personally prefer you to use VS Code tho")
+
+
+
+
+
+#Menu (Stable)
+print("")
+print("THE ENCHANTED DETECTICE")
+print("-----------------------------------------------")
+print("")
+print("a) Start Game")
+print("b) Options - Coming Soon...")
+print("c) Quick Notes - Coming Soon...")
+print("d) GitHub")
+print("")
+print("*Check GitHub For Any Updates")
+print("-----------------------------------------------")
+print("")
+MainMenu = input("Type a Letter Between A to D To Select a Option ¬ ")
+
+#Problem 1 (Unstable!)
+
+
+
+
+#Start Menu  - The Main Game     ! 
+if MainMenu == "A" or "a":
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("THE ENCHANTED DECTECTIVE: A PYTHON BASED DECISON GAME")
+    print("")
+    print("")
+    print("-----------------------------------------------------")
+    print("")
+    print("Warning: This Game is still in Early Development (a lot of bugs and fixes)")
+    print("")
+    print("-----------------------------------------------------")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+
+print("")
+print("! Quick Note Before You Start - Please just type the letterd (a, b and c) or numbers if your ask to")
+print("")
+EnteredContinue0 = input("Press Enter to Continue ¬ ")
+
+if EnteredContinue0 == "":
+
+    print("")
+    print("")
+    print("")
+    print("")
+    print("I'm Lilliana, a detective in the Ardenia police force. I've always been good at solving puzzles and getting to the bottom of things, but my real talent is something that most people would consider a liability:")
+    print("")
+    print("")
+    print("I can talk to animals. It's not something I go around advertising, but it comes in handy when I'm working on a case. Like this one, for example.")
+    print("")
+    print("")
+    print("I was called to the palace to investigate a string of disappearances that had been occurring in the gardens. The palace staff had been going missing one by one, and no one knew why. The palace guards had searched the gardens and the surrounding area, but they hadn't found any clues.")
+    print("")
+    print("")
+    print("As soon as I arrived, I could tell that something was off. The palace staff were all on edge, and the palace guards seemed more like they were guarding a prison than a royal residence. And then there was the ghost. Everyone had heard the rumors of the Ghost of the Rose Garden, but I had never taken them seriously. Until now. I could feel its presence everywhere I went, like a chill in the air. ")
+    print("")
+    print("")
+    print("I knew I had to get to the bottom of this case, but I didn't know where to start. That's when I remembered my special talent. I went to the gardens and started talking to the animals.")
+    print("")
+    EnteredContinue1 = input("Press Enter to Continue ¬ ")
+
+# Question 0 - Lilliana was talking to a group of birds. 
+
+if EnteredContinue1 =="":
+    print("")
+    print("")
+    print("")
+    print("Lilliana was talking to a group of birds")
+    print("---------------------------------------------")
+    print ("a) What's been going on here?")
+    print("b) What's up little birdie, you know whats is up?")
+    print("")
+    TypeQuestion0 = input("Type A or B Question: ")
+
+if TypeQuestion0 =="a" or "A" or "b" or "B":
+    print("")
+    print("---------------------------------------------")
+    print("")
+    print("The Birds: The ghost, the ghost")
+    print("")
+    print("---------------------------------------------")
+    print("")
+    print ("a) what do you know that ghost?")
+    print("b) .")
+    print("c) .")
+    print("")
+    TypeQuestion1 = input("Type 'A' Question: ")
+
+# Question 1 
+
+if TypeQuestion1 == "a" or "A":
+    print("")
+    print("---------------------------------------------")
+    print("")
+    print("Bird 1: it's been taking people, its comes in the night and takes them away")
+    print("")
+    EnteredContinue2 = input("Press Enter to continue ¬ ")
+
+if EnteredContinue2 == "": 
+    print("")
+    print("")
+    print("")
+    print("")
+    print("I thanked the birds for their help and went to talk to a group of squirrels. They told me that they had seen a figure in a hooded cloak hanging around the palace at night. ")
+    print("")
+    print("")
+    print("With this information, I returned to the palace and reported to Inspector Hans, my gruff but loyal partner on the force. He listened to my findings but was skeptical, insisting that the disappearances were just a publicity stunt. ")
+    print("")
+    print("")
+    print("I was determined to prove him wrong and put an end to the ghost's reign of terror. I knew it wouldn't be easy, but I was ready for the challenge. ")
+    print("")
+    print("")
+    print("The second chapter of my investigation began with a visit to the palace library. I wanted to learn more about the history of the ghost and see if there were any clues in the palace's records. ")
+    print("")
+    print("")
+    print("")
+    
